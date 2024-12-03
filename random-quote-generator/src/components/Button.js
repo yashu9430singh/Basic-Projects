9e16d32c0dc580ab7600e5b49556af587bf1318b
yashu9430singh <yashu9430@gmail.com>
2024-12-03 14:19:59 +0530

    Delete random-quote-generator directory

--- random-quote-generator/src/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({buttonDisplayName, clickHandler}) =>{
-    return(
-    <button onClick={clickHandler}>{buttonDisplayName}</button>
-    );
-};
-
-export default Button;
