76df296447d9065c2ba0171270912d6b913ecda8
Yashwant Singh <yashu9430@gmail.com>
2025-07-23 21:17:33 +0530

    Add files via upload

---
+++ BACKEND/FreeCodeCamp(Certification)/boilerplate-project-filemetadata/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const multer = require('multer');
+
+var app = express();
+const upload = multer({dest: 'uploads/'});
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res)=>{
+  if(!req.file){
+    res.status(400).json({error: 'No file uploaded'});
+  }
+
+  res.json({
+    name: req.file.originalname, 
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
